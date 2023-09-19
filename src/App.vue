02218de674d8a806e82fba72279124baeaddcc28
Aaron Maywood <am7274183@gmail.com>
2023-09-20 08:25:04 +0900

    Step6

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,28 @@
 <script setup>
 import Garden from './components/Garden.vue';
+import { isGameClear } from './store.js';
 </script>
 
 <template>
 	<h1>草むしり</h1>
 	<Garden />
+	<p class="clear" v-if="isGameClear()">GAME CLEAR!</p>
 </template>
 
 <style scoped>
 h1 {
 	text-align: center;
+}
+.clear {
+	background: #006600;
+	color: white;
+	padding: 1rem;
+	font-size: 5rem;
+	width: max-content;
+	/* 画面中央に表示 */
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%,-100%);
 }
 </style>
--- src/store.js
+++ src/store.js
@@ -1,5 +1,15 @@
 import { ref } from 'vue';
 
+export const gameState = ref(0);
+
+function gameClear(){
+	gameState.value = 1;
+}
+
+export function isGameClear(){
+	return gameState.value === 1;
+}
+
 const X_MAX = 5;
 const Y_MAX = 5;
 export const state = ref([
@@ -28,13 +38,29 @@ function grow(x,y){
 
 // 草を更新
 function update(){
+	// ゲームクリアーになったら以後更新をストップ
+	if (isGameClear()) return;
+
 	// 1. ランダムに空き地に草が生える
 	if( Math.random() > 0.985 ){
 		const x = Math.floor(Math.random() * X_MAX);
 		const y = Math.floor(Math.random() * Y_MAX);
 		grow(x,y);
 	}
-	
+
+	// もし、草が一本もなくなったらゲームクリアー
+	let grassCounter = 0;
+	for(let x=0 ; x < X_MAX ; x++){
+		for(let y=0 ; y < Y_MAX ; y++){
+			if( state.value[x][y] !== 0 ){
+				grassCounter++;
+			}
+		}
+	}
+	if(grassCounter === 0){
+		gameClear();
+	}
+
 	requestAnimationFrame(update);
 }
 
