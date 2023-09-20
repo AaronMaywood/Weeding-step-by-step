0c96015117b8aadf09527d331c25fd43450b83fd
Aaron Maywood <am7274183@gmail.com>
2023-09-20 12:03:14 +0900

    Step8 完成

--- src/store.js
+++ src/store.js
@@ -47,6 +47,7 @@ function grow(x,y){
 }
 
 // 草を更新
+let counter = 0;
 function update(){
 	// ゲームクリアーになったら以後更新をストップ
 	if (isGameClear()) return;
@@ -58,7 +59,30 @@ function update(){
 		grow(x,y);
 	}
 
-	// もし、草が一本もなくなったらゲームクリアー
+	// 2. 周囲に草が茂っているときは（根が張っているので）ぐんと伸びる＝緑が濃いところほど濃くなる
+	if( counter++ % 150 === 0 ){	 // 一定間隔毎に更新
+		// 次のコマの計算のため、現在の state.value をコピーした next を用意する
+		let next = JSON.parse(JSON.stringify(state.value))
+
+		for(let x=0 ; x < X_MAX ; x++){
+			for(let y=0 ; y < Y_MAX ; y++){
+				// 隣り合った上下左右の草の数を数える（枠外は数えない）
+				let counter = upper(x,y) + bottom(x,y) + right(x,y) + left(x,y);
+				// 上下左右の草の合計が4以上のとき、現在の位置に草を生やす
+				if(counter>4){
+					next[x][y]++;
+					if(next[x][y] > 3) {
+						next[x][y] = 3;
+					}
+				}
+			}
+		}
+
+		// 計算済みの next を現在のコマに反映
+		state.value = JSON.parse(JSON.stringify(next));
+	}
+
+	// 3. もし、草が一本もなくなったらゲームクリアー
 	let grassCounter = 0;
 	for(let x=0 ; x < X_MAX ; x++){
 		for(let y=0 ; y < Y_MAX ; y++){
@@ -76,3 +100,23 @@ function update(){
 
 requestAnimationFrame(update);
 
+// 上下左右の方向に草があるかを調べる
+function upper(x,y){
+	if(y <= 0){ return 0 }
+	return state.value[x][y-1]
+}
+
+function bottom(x,y){
+	if(y >= Y_MAX-1){ return 0 }
+	return state.value[x][y+1]
+}
+
+function right(x,y){
+	if(x >= X_MAX-1){ return 0 }
+	return state.value[x+1][y]
+}
+
+function left(x,y){
+	if(x <= 0){ return 0 }
+	return state.value[x-1][y]
+}
